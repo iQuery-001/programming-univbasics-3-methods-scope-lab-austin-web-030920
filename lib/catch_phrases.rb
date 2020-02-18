de3ae5db838071422079ba3faae89a28a7665b58
Dakota Willingham <kota@vaga.tk>
2020-02-18 20:33:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,12 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  sword = "It's Dangerous To Go Alone! Take This."
+  puts sword
+end
+
+def any_phrase(phrase)
+  puts phrase
+end
